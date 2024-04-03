1d94aee1b05044c655a1a04d9d212a3375d1e750
mattchunghk <mattchunghk@gmail.com>
2024-04-03 20:14:10 +0800

    magic id for get result

--- python/src/mt5_tradingbot_mac.py
+++ python/src/mt5_tradingbot_mac.py
@@ -413,7 +413,7 @@ def get_forward_test_result(symbol_ft, symbol_bt, start_date, end_date, initial_
     test_start_time = time.time()
     print(f"Test start - {len(class_history_deals)}")
     for deal in class_history_deals:
-        print('deal: ', deal)
+        # print('deal: ', deal)
         steps_completed += 1
         if progress_callback:
             elapsed_time, estimated_remaining_time = calculate_time_metrics(test_start_time, steps_completed, total_steps)
@@ -421,11 +421,11 @@ def get_forward_test_result(symbol_ft, symbol_bt, start_date, end_date, initial_
             progress_callback(progress_percentage, elapsed_time, estimated_remaining_time)
         
         deal_item = deal   
-        print('deal_item.magic : ', deal_item.magic == magic)
-        print('magic: ', magic)
-        print('test_id: ', test_id)
-        print('deal_item.comment: ', deal_item.comment)
-        print('deal_item.comment: ', deal_item.comment!=test_id)
+        # print('deal_item.magic : ', deal_item.magic == magic)
+        # print('magic: ', magic)
+        # print('test_id: ', test_id)
+        # print('deal_item.comment: ', deal_item.comment)
+        # print('deal_item.comment: ', deal_item.comment!=test_id)
         if deal_item.magic == magic and deal_item.comment != test_id:
             
             
