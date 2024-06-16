c1d3c7e9f94b55787601bddbda67ada389cda621
mattchunghk <mattchunghk@gmail.com>
2024-06-16 19:43:19 +0800

    tg enable

--- python/src/server.py
+++ python/src/server.py
@@ -894,11 +894,11 @@ def start_test():
         tg_invite_link = test_instance.tg_invite_link
         
         try:
-            tg_invite_link = loop.run_until_complete(generate_invite_link(client, tg_channel_id))
+            # tg_invite_link = loop.run_until_complete(generate_invite_link(client, tg_channel_id))
             if tg_channel_id == None and test_instance.tg_enable:
                 result = loop.run_until_complete(create_tg_channel(client, f'Invest Trend - {test_instance.test_name} (id#{test_instance.test_id})'))
                 tg_channel_id = test_instance.tg_channel_id = result
-                if tg_channel_id and tg_invite_link == None:
+            if tg_channel_id:
                     tg_invite_link = loop.run_until_complete(generate_invite_link(client, tg_channel_id))
                 # loop.run_until_complete(add_user_to_channel(client, tg_channel_id, test_instance.tg_username))
         except Exception as e:
