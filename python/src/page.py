c4c218fa3443ebee923221337c5d88bdd242d99c
mattchunghk <mattchunghk@gmail.com>
2024-07-21 17:59:02 +0800

    html update

--- python/src/page.py
+++ python/src/page.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/app')
-def redirect_to_port_3000():
-    return redirect("http://0.0.0.0:3000", code=302)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=4000, debug=False, use_reloader=False)
