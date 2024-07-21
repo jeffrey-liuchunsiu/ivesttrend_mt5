4f0eee98ca23c50474def0c3fff9fd27c98780d8
mattchunghk <mattchunghk@gmail.com>
2024-07-21 15:46:53 +0800

    port update

--- python/src/page.py
+++ python/src/page.py
@@ -7,4 +7,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000,debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=4000,debug=False, use_reloader=False)
