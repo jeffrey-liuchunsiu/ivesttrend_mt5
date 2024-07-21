7e8e315c3183cbce5e23e3ae847ec03c0e5b4233
mattchunghk <mattchunghk@gmail.com>
2024-07-21 17:55:13 +0800

    html update

--- python/src/page.py
+++ python/src/page.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/app')
+def redirect_to_port_3000():
+    return redirect("http://0.0.0.0:3000", code=302)
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=4000,debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=4000, debug=False, use_reloader=False)
