10f72f9f0cac82b4f45b9c3a9bb3fd14d7d52f3c
mattchunghk <mattchunghk@gmail.com>
2024-10-18 16:42:25 +0800

    add test

--- python/src/band_strategy/mt5_test_server.py
+++ python/src/band_strategy/mt5_test_server.py
@@ -86,5 +86,10 @@ def trade_result():
     # Here you can add code to process or store the trade result
     return jsonify({"message": "Trade result received"}), 200
 
+@app.route('/test', methods=['GET'])
+def test():
+    """Simple test endpoint."""
+    return jsonify({"message": "Test successful"}), 200
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
